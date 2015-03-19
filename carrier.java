442dd3cb004377fc1b9d7c5d4c0aa7e0dab02a53
karanamsubbarao <karanamsubbarao@gmail.com>
2015-03-19 07:04:32 +0530

    Jira-Plugin

---
+++ carrier.java
@@ -0,0 +1,33 @@
+/**
+ * This is the singular object for this custom field type.
+ * It has a Double field for amounts and a String field
+ * for comments about the the amount.
+ */
+public class Carrier {
+    
+    private Double amount;
+    private String note;
+    
+    public Carrier(Double amount, String note) {
+        this.amount = amount;
+        this.note = note;
+    }
+    
+    public Double getAmount() {
+        return amount;
+    }
+    
+    public String getNote() {
+        return note;
+    }
+    
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Carrier object: ");
+        sb.append(amount);
+        sb.append(" and ");
+        sb.append(note);
+        return sb.toString();
+    }
+    
+}
